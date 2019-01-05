2c419f889bd21dc8b4ef6bff5e4e38c262871677
Tim Allen <tnelsallen3@gmail.com>
2019-01-05 08:19:11 -0700

    Adds CEO method arch comments

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -1,2 +1,10 @@
 class Encryptor
+  def self.encrypt(message, key, date)
+    # CEO method
+    # key turns into ABCD keys
+    # date turns into ABCD offsets
+    # shift_synthesis turns keys and offsets into final shift
+    # final shift ABCD operates on message
+    # returns encrypted message
+  end
 end
