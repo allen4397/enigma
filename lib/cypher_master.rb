6d72ef7fa2b2e67a7bf961379a1ab63a2ee3baa9
Tim Allen <tnelsallen3@gmail.com>
2019-01-06 09:41:33 -0700

    Adds array of alphabet as instance variable to cypher master class

--- lib/cypher_master.rb
+++ lib/cypher_master.rb
@@ -1,2 +1,7 @@
 class CypherMaster
+  attr_reader :characters
+
+  def initialize
+    @characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+  end
 end
