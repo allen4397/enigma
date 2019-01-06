26c9b748d703efb002931a9292ef6cb4659096e9
Tim Allen <tnelsallen3@gmail.com>
2019-01-06 13:28:26 -0700

    Moves tests into cypher master test makes CypherMaster adjust_index the default one

--- lib/cypher_master.rb
+++ lib/cypher_master.rb
@@ -8,4 +8,13 @@ class CypherMaster
   def initialize
     @characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
   end
+
+  def adjust_index(char, shift)
+    cypher_index = @characters.find_index(char)
+    index = cypher_index + shift
+    while index > 26
+      index -= 27
+    end
+    index
+  end
 end
