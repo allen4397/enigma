fcd360bf01b7b833c1e80f649a7b22d3754da04b
Tim Allen <tnelsallen3@gmail.com>
2019-01-06 08:42:45 -0700

    Adds decrypt test to enigma

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -23,4 +23,18 @@ class EnigmaTest < Minitest::Test
 
     assert_equal expected, actual
   end
+
+  def test_it_can_decrypt_a_message
+    enigma = Enigma.new
+
+    expected = {
+      decryption: "hello world",
+      key: "02715",
+      date: "040895"
+    }
+
+    actual = enigma.decrypt("keder ohulw", "02715", "040895")
+
+    assert_equal expected, actual
+  end
 end
