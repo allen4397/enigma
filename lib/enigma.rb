27e333de3bc0ac2b85c78c76f33c33a52fe6de30
Tim Allen <tnelsallen3@gmail.com>
2019-01-06 08:32:39 -0700

    Adjusts message key in Enigma class to encryption

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,7 +3,7 @@ require_relative 'encryptor'
 class Enigma
   def encrypt(message, key, date)
     {
-      message: Encryptor.encrypt(message, key, date),
+      encryption: Encryptor.encrypt(message, key, date),
       key: key,
       date: date
     }
