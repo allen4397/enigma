c0de62034a8473197a1be3cda45ee059ca58fd7f
Tim Allen <tnelsallen3@gmail.com>
2019-01-06 09:40:48 -0700

    Adds test_it_starts_with_array_of_characters_of_alphabet to cypher master test

--- test/cypher_master_test.rb
+++ test/cypher_master_test.rb
@@ -9,4 +9,12 @@ class CypherMasterTest < Minitest::Test
 
     assert_instance_of CypherMaster, cypher_master
   end
+
+  def test_it_starts_with_array_of_characters_of_alphabet
+    cypher_master = CypherMaster.new
+
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
+    assert_equal expected, cypher_master.characters
+  end
 end
