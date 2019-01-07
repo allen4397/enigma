66555cf324f67e49a012950f66e0c74556384c49
Tim Allen <tnelsallen3@gmail.com>
2019-01-07 11:28:32 -0700

    adds code_breaker find_key method

--- lib/code_cracker.rb
+++ lib/code_cracker.rb
@@ -1,2 +1,14 @@
-class CodeCracker
+require_relative 'cypher_master'
+require_relative 'decryptor'
+require_relative 'key_generator'
+
+class CodeCracker < CypherMaster
+  def self.find_key(encryption, date)
+    decryption = ""
+    while decryption[-4..-1] != " end"
+      key = KeyGenerator.generate
+      decryption = Decryptor.decrypt(encryption, key, date)
+    end
+    return key
+  end
 end
