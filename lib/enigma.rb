48882e98fab8d3ae901e01278196b9a754bae09f
Tim Allen <tnelsallen3@gmail.com>
2019-01-07 12:23:05 -0700

    Adds crack test for today's date

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,7 @@
 require_relative 'encryptor'
 require_relative 'decryptor'
 require_relative 'key_generator'
+require_relative 'code_cracker'
 
 class Enigma
   def encrypt(message, key = KeyGenerator.generate, date = KeyGenerator.generate_date)
