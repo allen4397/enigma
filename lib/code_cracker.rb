61364e2277eb4685d9fd2f8b1e7b0629be487790
Tim Allen <tnelsallen3@gmail.com>
2019-01-07 14:44:07 -0700

    Removes extra methods, alters test to create encryption using todays date in order to decrypt it

--- lib/code_cracker.rb
+++ lib/code_cracker.rb
@@ -4,32 +4,6 @@ require_relative 'key_generator'
 
 class CodeCracker < CypherMaster
   def self.find_key(encryption, date)
-    code_cracker = CodeCracker.new
-    if date == KeyGenerator.generate_date
-      code_cracker.crack_challenge(encryption, date)
-    else
-      code_cracker.quick_crack(encryption, date)
-    end
-  end
-
-  def crack_challenge(encryption, date)
-    # get shifts somehow
-    offsets = transform_date(date)
-    keys = key_builder(shifts, offsets)
-    key = keys[:A] + keys[:C] + keys[:D][1]
-    return key
-  end
-
-  def key_builder(shifts, offsets)
-    keys = {}
-    keys[:A] = shifts[:A] - offsets[:A]
-    keys[:B] = shifts[:B] - offsets[:B]
-    keys[:C] = shifts[:C] - offsets[:C]
-    keys[:D] = shifts[:D] - offsets[:D]
-    return keys
-  end
-
-  def quick_crack(encryption, date)
     decryption = ""
     while decryption[-4..-1] != " end"
       key = KeyGenerator.generate
