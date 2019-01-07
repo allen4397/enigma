c4188c4bd4ef547b4e723f64473bf39867b22095
Tim Allen <tnelsallen3@gmail.com>
2019-01-07 12:57:11 -0700

    Refactors quick_crack out of find_key method

--- lib/code_cracker.rb
+++ lib/code_cracker.rb
@@ -4,6 +4,18 @@ require_relative 'key_generator'
 
 class CodeCracker < CypherMaster
   def self.find_key(encryption, date)
+    code_cracker = CodeCracker.new
+    if date == KeyGenerator.generate_date
+      code_cracker.crack_challenge(encryption, date)
+    else
+      code_cracker.quick_crack(encryption, date)
+    end
+  end
+
+  def crack_challenge(encryption, date)
+  end
+
+  def quick_crack(encryption, date)
     decryption = ""
     while decryption[-4..-1] != " end"
       key = KeyGenerator.generate
