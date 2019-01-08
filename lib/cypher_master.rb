977ef2ad3920291a407e25c36046e5e183801f70
Tim Allen <tnelsallen3@gmail.com>
2019-01-07 17:42:12 -0700

    Reset back to normal the adjust_index location

--- lib/cypher_master.rb
+++ lib/cypher_master.rb
@@ -9,4 +9,13 @@ class CypherMaster
   def initialize
     @characters = ("a".."z").to_a << " "
   end
+
+  def adjust_index(char, shift)
+    cypher_index = @characters.find_index(char)
+    index = cypher_index + shift
+    while index > 26
+      index -= 27
+    end
+    index
+  end
 end
--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -6,13 +6,4 @@ class Encryptor < CypherMaster
     shifts = encryptor.create_shifts(key, date)
     encryptor.master_shift(message, shifts)
   end
-
-  def adjust_index(char, shift)
-    cypher_index = @characters.find_index(char)
-    index = cypher_index + shift
-    while index > 26
-      index -= 27
-    end
-    index
-  end
 end
