ec503af8d0e84a736c17ffd9fdcdec90ee321502
Tim Allen <tnelsallen3@gmail.com>
2019-01-07 17:32:32 -0700

    Actually adds the methods to the new module

--- lib/cypher_master.rb
+++ lib/cypher_master.rb
@@ -9,13 +9,4 @@ class CypherMaster
   def initialize
     @characters = ("a".."z").to_a << " "
   end
-
-  def adjust_index(char, shift)
-    cypher_index = @characters.find_index(char)
-    index = cypher_index + shift
-    while index > 26
-      index -= 27
-    end
-    index
-  end
 end
